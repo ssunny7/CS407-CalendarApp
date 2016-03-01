36ff7c34a2ccd7acd3f2921ed1d15b0cab6525ed
Sunny Shah <shahsunny712@gmail.com>
2016-03-01 00:09:35 -0600

    Initial commit.
    Minimum Android version: 5.1
    
    Added start activity consisting of a calendar view widget.

---
+++ app/src/main/java/com/ssunny/cs407/calendar/DayPicker.java
@@ -0,0 +1,24 @@
+package com.ssunny.cs407.calendar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.CalendarView;
+
+public class DayPicker extends AppCompatActivity {
+
+    private CalendarView calendarView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_day_picker);
+
+        calendarView = (CalendarView)findViewById(R.id.calendarView);
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
+                
+            }
+        });
+    }
+}
